63f1d5af18e44bab01525f5da34b934e0d4aba16
alene <alene.rhea@gmail.com>
2020-04-25 00:07:17 -0400

    train_val_test_split rewrite with sampleBy

--- data_prep.py
+++ data_prep.py
@@ -155,32 +155,40 @@ def train_val_test_split(spark, data, seed=42):
         especially after partitioning their observations into train/test.
         You may discard these users from the experiment.
 
-        speed up queries with user_id & book_id indexes?
+        could/should we speed up queries with user_id & book_id indexes?
     '''
     users=data.select('user_id').distinct()
     users_train, users_val, users_test = users.randomSplit([0.6, 0.2, 0.2], seed=seed)
     
     users_train.createOrReplaceTempView('users_train')
     users_val.createOrReplaceTempView('users_val')
-    users_val.createOrReplaceTempView('users_test')
+    users_test.createOrReplaceTempView('users_test')
     data.createOrReplaceTempView('data')
     
     #Training Set - 60% of users and all interactions
     train = spark.sql('SELECT users_train.user_id, book_id, rating FROM users_train LEFT JOIN data on users_train.user_id=data.user_id')
 
-    #Validation Set - 20% of users and half their interactions (half back into training)
+    #Validation Set - 20% of users
     val_all = spark.sql('SELECT users_val.user_id, book_id, rating FROM users_val LEFT JOIN data on users_val.user_id=data.user_id')
-    val, val_to_train = val_all.randomSplit([0.5, 0.5], seed=seed)
-    
-    #concatenate rows from val_to_train to train
-    train=train.unionAll(val_to_train)
 
-    #Test Set - remaining 20% of users and half their interactions (half back into training)
+    # Sample 50% of interactions from each user in val_all, to keep for val set
+    val_dict = val_all.select(val_all.user_id).distinct().rdd.map(lambda x : (x[0], 0.5).collectAsMap()
+    val = val_all.sampleBy("user_id", fractions=val_dict, seed=seed)
+
+    #Put other 50% of interactions back into train
+    val_to_train = val_all.except(val)
+    train=train.union(val_to_train).distinct() 
+
+    #Test Set - 20% of users
     test_all = spark.sql('SELECT users_test.user_id, book_id, rating FROM users_test LEFT JOIN data on users_test.user_id=data.user_id')
-    test, test_to_train = test_all.randomSplit([0.5, 0.5], seed=seed)
 
-    #concatenate rows from test_to_train to train
-    train=train.unionAll(test_to_train)
+    # Sample 50% of interactions from each user in test_all, to keep for test set
+    test_dict = test_all.select(test_all.user_id).distinct().rdd.map(lambda x : (x[0], 0.5).collectAsMap()
+    test = test_all.sampleBy("user_id", fractions=test_dict, seed=seed)
+
+    #Put other 50% of interactions back into train
+    test_to_train = test_all.except(test)
+    train=train.union(test_to_train).distinct() 
 
     return train, val, test
 
