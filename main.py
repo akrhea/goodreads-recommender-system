6aff58588a1db369f66e61db2abafdbdb7ce9f7f
alene <alene.rhea@gmail.com>
2020-05-11 14:40:00 -0400

    hivecontext via sparkcontext

--- main.py
+++ main.py
@@ -71,7 +71,9 @@ def main(spark, task, fraction, k):
         return
     
     if task=='test':
-        from pyspark.sql.hive import HiveContext
+        import pyspark
+        sc = pyspark.SparkContext()
+        hiveContext = pyspark.sql.HiveContext(sc)
 
         # best hyperparameteters
         best_rank = 500
@@ -79,8 +81,8 @@ def main(spark, task, fraction, k):
         best_isrev_weight = -1
 
         # uncache train and val
-        HiveContext.uncacheTable("train")
-        HiveContext.uncacheTable("val")
+        hiveContext.uncacheTable("train")
+        hiveContext.uncacheTable("val")
 
         # reassign train and val
         train = train.union(val)
